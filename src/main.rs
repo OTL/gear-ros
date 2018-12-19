55e0641e84c014fa131cc10ffeeb4f8150ebda0a
Takashi Ogura <t.ogura@gmail.com>
2018-12-19 10:44:19 +0000

    Update gear

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ extern crate gear;
 extern crate glfw;
 extern crate k;
 extern crate nalgebra as na;
-extern crate ncollide;
+extern crate ncollide3d;
 extern crate urdf_viz;
 extern crate urdf_rs;
 //#[macro_use]
@@ -28,9 +28,9 @@ extern crate rosrust_codegen;
 
 use std::time::{Duration, Instant};
 use std::sync::mpsc;
-use k::JointContainer;
+use gear::FromUrdf;
 use glfw::{Action, Key, WindowEvent};
-use ncollide::shape::Compound3;
+use ncollide3d::shape::Compound;
 
 rosmsg_include!();
 
@@ -38,33 +38,43 @@ struct CollisionAvoidApp<I>
 where
     I: gear::InverseKinematicsSolver<f64>,
 {
-    planner: gear::JointPathPlannerWithIK<I>,
-    obstacles: Compound3<f64>,
+    planner: gear::JointPathPlannerWithIK<f64, I>,
+    obstacles: Compound<f64>,
     ik_target_pose: na::Isometry3<f64>,
     colliding_link_names: Vec<String>,
     viewer: urdf_viz::Viewer,
-    arm: k::LinkChain<f64>,
+    arm: k::SerialChain<f64>,
+    end_link_name: String,
 }
 
 impl<I> CollisionAvoidApp<I>
 where
     I: gear::InverseKinematicsSolver<f64>,
 {
-    fn new(planner: gear::JointPathPlannerWithIK<I>, end_link_name: &str) -> Self {
+    fn new(planner: gear::JointPathPlannerWithIK<f64, I>, end_link_name: &str) -> Self {
         let mut viewer = urdf_viz::Viewer::new("gear: example reach");
         viewer.add_robot(planner.urdf_robot().as_ref().unwrap());
         viewer.add_axis_cylinders("origin", 1.0);
 
         let urdf_obstacles = urdf_rs::utils::read_urdf_or_xacro("obstacles.urdf")
             .expect("obstacle file not found");
-        let obstacles = gear::create_compound_from_urdf_robot(&urdf_obstacles);
+        let obstacles = Compound::from_urdf_robot(&urdf_obstacles);
         viewer.add_robot(&urdf_obstacles);
 
         let ik_target_pose = na::Isometry3::from_parts(
             na::Translation3::new(0.40, 0.20, 0.3),
-            na::UnitQuaternion::from_euler_angles(0.0, 1.57, 1.57),
+            na::UnitQuaternion::from_euler_angles(0.0, -0.1, 0.0),
         );
-        let arm = planner.create_arm(end_link_name).unwrap();
+        println!("{}", planner.path_planner.collision_check_robot);
+        let arm = {
+            let end_link = planner
+                .path_planner
+                .collision_check_robot
+                .find(end_link_name)
+                .expect(&format!("{} not found", end_link_name));
+            k::SerialChain::from_end(end_link)
+        };
+        let end_link_name = end_link_name.to_owned();
         viewer.add_axis_cylinders("ik_target", 0.3);
         CollisionAvoidApp {
             viewer,
@@ -73,18 +83,27 @@ where
             colliding_link_names: Vec::new(),
             planner,
             arm,
+            end_link_name,
         }
     }
     fn update_robot(&mut self) {
-        self.viewer.update(&self.planner);
+        // this is hack to handle invalid mimic joints
+        let ja = self
+            .planner
+            .path_planner
+            .collision_check_robot
+            .joint_positions();
+        self.planner
+            .path_planner
+            .collision_check_robot
+            .set_joint_positions(&ja)
+            .unwrap();
+        self.viewer
+            .update(&self.planner.path_planner.collision_check_robot);
     }
     fn update_ik_target(&mut self) {
-        if let Some(obj_vec) = self.viewer.scenes.get_mut("ik_target") {
-            for obj in obj_vec {
-                obj.0.set_local_transformation(
-                    na::convert(self.ik_target_pose),
-                )
-            }
+        if let Some(obj) = self.viewer.scene_node_mut("ik_target") {
+            obj.set_local_transformation(na::convert(self.ik_target_pose));
         }
     }
     fn reset_colliding_link_colors(&mut self) {
@@ -110,18 +129,18 @@ where
         while self.viewer.render() && rosrust::is_ok() {
             if plans.is_empty() {
                 if let Ok(msg) = rx.recv_timeout(Duration::from_millis(10)) {
-                    let mut angles = self.arm.joint_angles();
-                    for (i, name) in self.arm.joint_names().into_iter().enumerate() {
+                    let mut angles = self.arm.joint_positions();
+                    for (i, name) in self.arm.iter_joints().map(|j| j.name.to_owned()).enumerate() {
                         let msg_names = msg.name.clone();
                         if let Some(pos) = msg_names.into_iter().position(|n| n == name) {
                             angles[i] = msg.position[pos];
                         }
                     }
-                    self.arm.set_joint_angles(&angles).unwrap();
+                    self.arm.set_joint_positions(&angles).unwrap();
                     self.update_robot();
                 }
             } else {
-                self.arm.set_joint_angles(&plans.pop().unwrap()).unwrap();
+                self.arm.set_joint_positions(&plans.pop().unwrap()).unwrap();
                 self.update_robot();
             }
             for event in self.viewer.events().iter() {
@@ -129,7 +148,7 @@ where
                     WindowEvent::Key(code, _, Action::Press, mods) => {
                         match code {
                             Key::Up => {
-                                if mods.contains(glfw::modifiers::Shift) {
+                                if mods.contains(glfw::Modifiers::Shift) {
                                     self.ik_target_pose.rotation *=
                                         na::UnitQuaternion::from_euler_angles(0.0, 0.0, 0.2);
                                 } else {
@@ -138,7 +157,7 @@ where
                                 self.update_ik_target();
                             }
                             Key::Down => {
-                                if mods.contains(glfw::modifiers::Shift) {
+                                if mods.contains(glfw::Modifiers::Shift) {
                                     self.ik_target_pose.rotation *=
                                         na::UnitQuaternion::from_euler_angles(0.0, 0.0, -0.2);
                                 } else {
@@ -147,7 +166,7 @@ where
                                 self.update_ik_target();
                             }
                             Key::Left => {
-                                if mods.contains(glfw::modifiers::Shift) {
+                                if mods.contains(glfw::Modifiers::Shift) {
                                     self.ik_target_pose.rotation *=
                                         na::UnitQuaternion::from_euler_angles(0.0, 0.2, -0.0);
                                 } else {
@@ -156,7 +175,7 @@ where
                                 self.update_ik_target();
                             }
                             Key::Right => {
-                                if mods.contains(glfw::modifiers::Shift) {
+                                if mods.contains(glfw::Modifiers::Shift) {
                                     self.ik_target_pose.rotation *=
                                         na::UnitQuaternion::from_euler_angles(0.0, -0.2, 0.0);
                                 } else {
@@ -165,7 +184,7 @@ where
                                 self.update_ik_target();
                             }
                             Key::B => {
-                                if mods.contains(glfw::modifiers::Shift) {
+                                if mods.contains(glfw::Modifiers::Shift) {
                                     self.ik_target_pose.rotation *=
                                         na::UnitQuaternion::from_euler_angles(-0.2, 0.0, 0.0);
                                 } else {
@@ -174,7 +193,7 @@ where
                                 self.update_ik_target();
                             }
                             Key::F => {
-                                if mods.contains(glfw::modifiers::Shift) {
+                                if mods.contains(glfw::Modifiers::Shift) {
                                     self.ik_target_pose.rotation *=
                                         na::UnitQuaternion::from_euler_angles(0.2, 0.0, 0.0);
                                 } else {
@@ -195,7 +214,7 @@ where
                             Key::G => {
                                 self.reset_colliding_link_colors();
                                 match self.planner.plan_with_ik(
-                                    &mut self.arm,
+                                    &self.end_link_name,
                                     &self.ik_target_pose,
                                     &self.obstacles,
                                 ) {
@@ -213,7 +232,7 @@ where
                                         msg.goal_id.id = format!("gear_ros{:?}", Instant::now());
                                         let mut traj_msg =
                                             msg::trajectory_msgs::JointTrajectory::default();
-                                        traj_msg.joint_names = self.arm.joint_names();
+                                        traj_msg.joint_names = self.arm.iter_joints().map(|j| j.name.to_owned()).collect();
                                         for (i, trajectory_point) in
                                             trajectory_points.into_iter().enumerate()
                                         {
@@ -242,7 +261,7 @@ where
                             }
                             Key::R => {
                                 self.reset_colliding_link_colors();
-                                gear::set_random_joint_angles(&mut self.arm).unwrap();
+                                gear::set_random_joint_positions(&mut self.arm).unwrap();
                                 self.update_robot();
                             }
                             Key::C => {
@@ -264,7 +283,8 @@ where
                                     None,
                                     is_collide_show,
                                 );
-                                self.viewer.update(&self.planner);
+                                self.viewer
+                                .update(&self.planner.path_planner.collision_check_robot);
                             }
                             _ => {}
                         }
@@ -280,18 +300,13 @@ fn main() {
     use std::env;
     env_logger::init().unwrap();
     let input_string = env::args().nth(1).unwrap_or("sample.urdf".to_owned());
-    let input_end_link = env::args().nth(2).unwrap_or("l_tool".to_owned());
+    let input_end_link = env::args().nth(2).unwrap_or("l_tool_fixed".to_owned());
     let planner = gear::JointPathPlannerBuilder::from_urdf_file(&input_string)
         .unwrap()
         .collision_check_margin(0.01)
         .finalize();
-    let solver = gear::JacobianIKSolverBuilder::<f64>::new()
-        .num_max_try(1000)
-        .allowable_target_distance(0.01)
-        .move_epsilon(0.00001)
-        .jacobian_move_epsilon(0.001)
-        .finalize();
-    let solver = gear::RandomInitializeIKSolver::new(solver, 100);
+    let solver = gear::JacobianIKSolver::new(0.01, 0.05, 0.5, 100);
+    let solver = gear::RandomInitializeIKSolver::new(solver, 10);
     let planner = gear::JointPathPlannerWithIK::new(planner, solver);
     let mut app = CollisionAvoidApp::new(planner, &input_end_link);
     app.run();
